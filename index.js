078dbbd89ef3d26c03c3d6ca41a4ba9da8d511b7
praveen291 <mudadlapraveenkumar143@gmail.com>
2021-12-30 21:59:32 +0530

    multiplay

--- index.js
+++ index.js
@@ -37,10 +37,17 @@
 
 // write a java script program to check the two numbers and return true if one of the numbers is 100 or if the sum of the two numbers is 100
 
-const isEqualTo100 = (a, b) => a === 100 || b === 100 || (a+b) === 100;
+// const isEqualTo100 = (a, b) => a === 100 || b === 100 || (a+b) === 100 ||(a-b)===100;
 
-console.log(isEqualTo100(100, 0));
-console.log(isEqualTo100(10, 90));
-console.log(isEqualTo100(80, 20));
-console.log(isEqualTo100(10, 100));
-console.log(isEqualTo100(100, 0));
+// console.log(isEqualTo100(100, 0));
+// console.log(isEqualTo100(10, 90));
+// console.log(isEqualTo100(80, 20));
+// console.log(isEqualTo100(10, 100));
+// console.log(isEqualTo100(100, 0));
+// console.log(isEqualTo100(110, 10));
+
+
+const isMultiply100 = (a, b) => a === 100 || b === 100 || (a*b)===100;
+
+console.log(isMultiply100(10, 10));
+console.log(isMultiply100(5, 10));
