b8ad6698390b2ba6af98928db3ceddc30448451f
praveen291 <mudadlapraveenkumar143@gmail.com>
2021-12-31 18:10:28 +0530

    call bind

---
+++ bind.js
@@ -0,0 +1,24 @@
+// Bind method
+// function submit() {
+//   console.log("praveen")
+// }
+// const userComponent = {
+//   render() {
+//     this.submit();
+//   }
+// };
+// let result = submit.bind(userComponent);
+// result();
+// call...................................................................
+// let person = {
+//   name:'praveen'
+// }
+
+
+// function sample(a, b, c) {
+//  console.log("sample", a, b, c,this.name);
+// }
+// sample.call(person, 1, 2, 3);
+
+
+
--- index.js
+++ index.js
@@ -34,7 +34,6 @@
 // var c = parseInt(a) * parseInt(b);
 // console.log(c);.............................................
 
-
 // write a java script program to check the two numbers and return true if one of the numbers is 100 or if the sum of the two numbers is 100
 
 // const isEqualTo100 = (a, b) => a === 100 || b === 100 || (a+b) === 100 ||(a-b)===100;
@@ -46,8 +45,44 @@
 // console.log(isEqualTo100(100, 0));
 // console.log(isEqualTo100(110, 10));
 
+// const isMultiply100 = (a, b) => a === 100 || b === 100 || (a*b)===100;
 
-const isMultiply100 = (a, b) => a === 100 || b === 100 || (a*b)===100;
+// console.log(isMultiply100(10, 10));
+// console.log(isMultiply100(5, 10));
 
-console.log(isMultiply100(10, 10));
-console.log(isMultiply100(5, 10));
+// write a js program to get the file extension................................................................
+
+// const fileExtension = (str) => str.slice(str.lastIndexOf('.'));
+// console.log(fileExtension('index.html'));
+
+// const fileExtension = (str) => str.slice(str.lastIndexOf('.'));
+// console.log(fileExtension('praveen.pk'))................................................................
+
+// write a javascript program  to get the current  date.
+
+// const formDate = (date = new Date()) => {
+//   const days = date.getDay();
+//   const month = date.getMonth();
+//   const year = date.getFullYear();
+//   const time = date.getHours();
+//   const minutes = date.getMinutes();
+//   const seconds = date.getSeconds();
+//   return `${days}/${month}/${year}-${time}-${minutes}-${seconds}`;
+// };
+
+// console.log(formDate());
+
+
+
+//  this key word
+
+let person = {
+  firstName: "praveen",
+  getName: function () {
+    console.log(this.firstName);
+  },
+};
+person.getName();
+// Call vs apply vs binding.................................
+
+
