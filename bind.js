2901f12c36763ef5dc41e1a2e435887f023b1807
praveen291 <mudadlapraveenkumar143@gmail.com>
2021-12-31 20:53:58 +0530

    obd

--- bind.js
+++ bind.js
@@ -47,12 +47,37 @@
 
 // callBack & promise................................
 
-const p1 = Promise.resolve('my name is praveen ');
-const p2 = Promise.reject('Rejected');
-const p3 = 100;
-const p4 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 1000, 'Subscribe more up dates')
-});
-Promise.race([p1, p3, p4]).then((value) => console.log(value))
-Promise.allSettled([p1, p3, p4]).then((value) => console.log(value));
-Promise.any([p1, p3, p4]).then((value) => console.log(value));
+// const p1 = Promise.resolve('my name is praveen ');
+// const p2 = Promise.reject('Rejected');
+// const p3 = 100;
+// const p4 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 1000, 'Subscribe more up dates')
+// });
+// Promise.race([p1, p3, p4]).then((value) => console.log(value))
+// Promise.allSettled([p1, p3, p4]).then((value) => console.log(value));
+// Promise.any([p1, p3, p4]).then((value) => console.log(value));
+
+
+// Array destructuring
+
+// const myprograminglang=['js', 'jsx', 'php','pyton']
+// let [a, b, c, d, ] = myprograminglang;
+// console.log(`My fav prog lang is ${a} and ${d}`)
+
+
+//object destructuring
+
+
+const employee = {
+  id: 1,
+  age: 25,
+  department: "UI/UX",
+  adress: {
+    city: "vspt",
+    country: "Ind"
+  },
+};
+
+const { name: fullName = "praveen kumar", age } = employee;
+console.log(fullName);
+console.log(age);
