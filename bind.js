d2e873155f5aac6ebc2b9d2a04f68d27ff00c5ce
praveen291 <mudadlapraveenkumar143@gmail.com>
2021-12-31 18:24:40 +0530

    apply

--- bind.js
+++ bind.js
@@ -21,4 +21,13 @@
 // sample.call(person, 1, 2, 3);
 
 
+// apply..........................................................
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
+// console.log(Math.max.apply(null,numbers))
+
+
+// const sample = (name) => {
+//   console.log("sample",name)
+// }
+// sample.apply(null,["praveen"])
